2297af1174e94d55d0ab1f8b20953212481ade56
burrack <liron.burrack@dailymotion.com>
2018-06-14 20:23:02 -0400

    start

---
+++ src/containers/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { withSiteData, Head } from 'react-static'
+//
+import logoImg from '../logo.png'
+
+export default withSiteData(({ title }) => (
+  <div>
+    <Head>{title} s</Head>
+    <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
+    <img src={logoImg} alt="" />
+  </div>
+))
