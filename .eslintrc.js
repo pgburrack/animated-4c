a8bd90e5c4149affec2cffeb6773f71aee962535
burrack <liron.burrack@dailymotion.com>
2018-06-16 20:56:49 -0400

    add eslint-rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-  extends: 'react-tools',
+  extends: ['react-tools', "plugin:prettier/recommended"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": ["error", {
+      "tabWidth": 2,
+      "useTabs": false,
+      "semi": true,
+      "singleQuote": true,
+      "trailingComma": "none",
+      "bracketSpacing": true,
+      "jsxBracketSameLine": true,
+      "requirePragma": false
+    }]
+  }
 }
