b275649e840cbec8f14cc40da4fde2394e72de17
burrack <liron.burrack@dailymotion.com>
2018-06-16 20:33:55 -0400

    - modify default Document to static.config.js - remove commas

--- static.config.js
+++ static.config.js
@@ -1,38 +1,58 @@
-import axios from 'axios'
+import React from 'react';
+import axios from 'axios';
 
 export default {
+  Document: ({ Html, Head, Body, children, siteData, renderMeta }) => (
+    <Html lang="en-US">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Spectral:300,400,500,600,700,800"
+          rel="stylesheet"
+        />
+      </Head>
+      <Body>{children}</Body>
+    </Html>
+  ),
   getSiteData: () => ({
-    title: 'React Static',
+    title: 'React Static'
   }),
   getRoutes: async () => {
-    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    const { data: posts } = await axios.get(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
     return [
       {
         path: '/',
-        component: 'src/containers/Home',
+        component: 'src/containers/Home'
       },
       {
         path: '/about',
-        component: 'src/containers/About',
+        component: 'src/containers/About'
       },
       {
         path: '/blog',
         component: 'src/containers/Blog',
         getData: () => ({
-          posts,
+          posts
         }),
         children: posts.map(post => ({
           path: `/post/${post.id}`,
           component: 'src/containers/Post',
           getData: () => ({
-            post,
-          }),
-        })),
+            post
+          })
+        }))
       },
       {
         is404: true,
-        component: 'src/containers/404',
-      },
-    ]
-  },
-}
+        component: 'src/containers/404'
+      }
+    ];
+  }
+};
