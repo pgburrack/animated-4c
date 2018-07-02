941915125b52dbe91d68ee1cf4adada024868c72
burrack <liron.burrack@dailymotion.com>
2018-07-01 22:59:57 -0400

    change hansel and gretel styles

--- src/containers/Project.js
+++ src/containers/Project.js
@@ -1,55 +1,68 @@
 import React from 'react';
 import styled from 'styled-components';
+import { fontSize, lineHeight, textAlign } from 'styled-system';
 import { Column, Row } from '../grid';
-//
 
 const Title = styled.h1`
-  font-size: 40px;
   font-weight: 600;
   font-style: normal;
   font-stretch: normal;
-  line-height: 1.2;
   letter-spacing: normal;
   text-align: right;
   color: ${({ theme }) => theme.colors.black};
-  margin-bottom: 22px;
-`;
-
-const ImmersiveStoryte = styled.div`
-  font-size: 20px;
-  font-weight: 500;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  text-align: right;
-  color: ${({ theme }) => theme.colors['greyish-brown']};
+  ${fontSize};
+  ${lineHeight};
+  ${textAlign};
 `;
 
 const HanselGretelIsA = styled(Column)`
-  font-size: 24px;
   font-weight: 500;
   font-style: normal;
   font-stretch: normal;
-  line-height: 1.33;
   letter-spacing: normal;
   color: #636363;
+  ${fontSize};
+  ${lineHeight};
 `;
 
+// col={[4, 8, 7, 9, 6, 6]}
 export default () => (
   <div>
-    <Row is="section">
-      <HanselGretelIsA auto={false}>
+    <Row
+      alignItems={[
+        'flex-end',
+        'flex-end',
+        'flex-end',
+        'flex-end',
+        'stretch',
+        'stretch'
+      ]}
+      flexDirection={[
+        'column-reverse',
+        'column-reverse',
+        'column-reverse',
+        'column-reverse',
+        'row',
+        'row'
+      ]}
+      is="section">
+      <HanselGretelIsA
+        width={[1, 1, 7 / 8, 9 / 12, 1 / 2, 1 / 2]}
+        lineHeight={[1.2, 1.2, 1.2, 1.2, 1.33, 1.33]}
+        fontSize={['20px', '20px', '20px', '20px', '24px', '24px']}
+        auto={false}>
         Hansel & Gretel is an immersive storytelling suite for content creators
         to analyze, optimize their 360 videos, through understanding how the
         viewers are deviating from the point of interest and guide them with
         Real-Time Visual cues.
       </HanselGretelIsA>
-      <Column auto={false}>
-        <Title>Hansel & Gretel</Title>
-        <ImmersiveStoryte>
-          HAn Immersive Storytelling Suite for 360 Degree Video
-        </ImmersiveStoryte>
+      <Column width={[1, 1, 7 / 8, 9 / 12, 1 / 2, 1 / 2]} auto={false}>
+        <Title
+          textAlign={['left', 'right', 'right']}
+          lineHeight={[1.33, 1.25, 1.25, 1.25, 1.2, 1.2]}
+          fontSize={['24px', '32px', '32px', '32px', '40px', '40px']}>
+          Hansel & Gretel
+        </Title>
       </Column>
     </Row>
     <p>Project</p>
