0743c02a05dcb44faa3da38bcb87548d9b9a6be0
burrack <liron.burrack@dailymotion.com>
2018-07-27 17:56:27 -0400

    make home page links vertical #12

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withSiteData, Head, Route, Switch, NavLink } from 'react-static';
-import { Column, Row } from '../grid';
+import { Column, Row, Flex } from '../grid';
 import Intro from '../home/Intro';
 import Projects from '../home/Projects';
 import Project from '../home/Project';
@@ -11,9 +11,11 @@ export default withSiteData(({ title }) => (
     flexDirection={['column', 'column', 'row', 'row']}>
     <Head>{title} Home page</Head>
     <Column width={4 / 12}>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/archives">Archives</NavLink>
-      <NavLink to="/archives/1">Project 1</NavLink>
+      <Flex flexDirection="column">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/archives">Archives</NavLink>
+        <NavLink to="/archives/1">Project 1</NavLink>
+      </Flex>
     </Column>
     <Column width={1 / 2}>
       <Switch>
