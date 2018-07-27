e303f825d2a7f84d0138eefc27a08de2e61ee7a7
pgburrack <pgburrack@users.noreply.github.com>
2018-07-27 18:05:53 -0400

    make home page links vertical #12 (#13)
    issue #12

--- src/grid/index.js
+++ src/grid/index.js
@@ -1,4 +1,5 @@
 export { default as Row } from './Row';
 export { default as Column } from './Column';
 export { default as Container } from './Container';
+export { default as Flex } from './Flex';
 export { default as theme } from './theme';
